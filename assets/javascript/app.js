0dbb999659d0e8c2002c3356e2979bf0c64e62cc
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-06 15:06:41 -0800

    pseudo-coding javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// form for creating buttons
+// div to place buttons in
+// dynamic buttons with unique values depending on user input
+// button on click listener
+//      * contains ajax call based on unique value from button, rating limited
+//          * for loop creating image elements and placing them on the page, with three data attributes (animated url, still url, and state of image)
+// image on click listener
+//      * if statement based on state data attribute
+//          * switches src attribute depending on state and then switches state
