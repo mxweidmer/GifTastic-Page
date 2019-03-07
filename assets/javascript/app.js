7a18f05145bac49dae620dad65234a1c9495dfe8
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-06 20:12:26 -0800

    added some basic styling, created function to create and display buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,6 @@
 
 
 // form for creating buttons
-// div to place buttons in
 // dynamic buttons with unique values depending on user input
 // button on click listener
 //      * contains ajax call based on unique value from button, rating limited
@@ -12,3 +11,26 @@
 // image on click listener
 //      * if statement based on state data attribute
 //          * switches src attribute depending on state and then switches state
+
+
+
+$(document).ready(function () {
+
+    var topics = ["fallout new vegas", "dogs", "cats", "photography", "pokemon"];
+
+    function createButtons() {
+        $("#buttons-div").empty();
+
+        for (var i = 0; i < topics.length; i++) {
+
+            var topicButton = $("<button>");
+            topicButton.addClass("gif-button");
+            topicButton.attr("data-topic", topics[i]);
+            topicButton.text(topics[i]);
+            $("#buttons-div").append(topicButton);
+        }
+    }
+
+    createButtons();
+
+});
