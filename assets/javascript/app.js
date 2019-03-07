317f2c3090c7538d749758ef8b666cecfbda0b7d
mxweidmer <44995486+mxweidmer@users.noreply.github.com>
2019-03-06 21:36:13 -0800

    added comments for better code readability

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 // form for creating buttons
 // dynamic buttons with unique values depending on user input
 // button on click listener
@@ -12,12 +7,12 @@
 //      * if statement based on state data attribute
 //          * switches src attribute depending on state and then switches state
 
-
-
 $(document).ready(function () {
 
     var topics = ["video games", "dogs", "cats", "reading", "rally racing"];
 
+    // function for creating buttons. loops through topics array, setting data-topic to the input
+    // for retrieval later in the ajax call
     function createButtons() {
         $("#buttons-div").empty();
 
@@ -31,12 +26,15 @@ $(document).ready(function () {
         }
     }
 
+    // click listener for topic buttons.  sets topic (q) in url to data-topic from button
     $(document).on("click", ".gif-button", function () {
 
         var topic = $(this).attr("data-topic");
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&limit=10&rating=pg&api_key=el1LWbh5MYri61KekZrPPTDZzDlVqfGw";
 
+        // ajax call. empties previous gif-div, then loops through returned results creating
+        // img and p elements to hold gif and rating respectively
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -45,16 +43,20 @@ $(document).ready(function () {
             $("#gifs-div").empty();
 
             for (var i = 0; i < results.length; i++) {
+                // using fixed height gif for better layout
                 var url_still = results[i].images.fixed_height_still.url;
                 var url_animated = results[i].images.fixed_height.url;
                 var rating = results[i].rating;
 
+                // new gifDiv created for every image to hold image and p tag together
+                // floated left for better layout on page (aligned side by side when possible)
                 var gifDiv = $("<div>");
                 gifDiv.css("float", "left");
                 gifDiv.addClass("gif-box")
 
                 var ratingP = $("<p>").text("Rating: " + rating);
 
+                // using data-attributes to hold both still and animated url links, as well as state
                 var gif = $("<img>").addClass("topic-gif");
                 gif.attr("data-still", url_still);
                 gif.attr("data-animated", url_animated);
@@ -67,6 +69,8 @@ $(document).ready(function () {
         });
     });
 
+    // click listener on images for switching between animated and still using data-attributes
+    // and if statement
     $(document).on("click", ".topic-gif", function () {
 
         if ($(this).attr("data-state") === "still") {
@@ -78,15 +82,15 @@ $(document).ready(function () {
         }
     });
 
+    // click listener for submit button. pushes new topic to array, clears text input field
+    // then recreates button with new topic included
     $(document).on("click", "#submit", function () {
 
         topics.push($("#user-input").val().trim());
-
         $("#user-input").val("");
-
         createButtons();
     });
 
+    // displays initial topics as buttons on page
     createButtons();
-
 });
